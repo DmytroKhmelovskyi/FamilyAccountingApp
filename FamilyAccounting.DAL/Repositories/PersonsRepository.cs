6194879a07e31cbb7ac6291e2a80eba5a7e60f86
Nazar Chura <gravityholdontome@gmail.com>
2021-03-17 16:32:23 +0200

    Added repository interfaces with their's implementation clasess

---
+++ FamilyAccounting.DAL/Repositories/PersonsRepository.cs
@@ -0,0 +1,11 @@
+﻿using FamilyAccounting.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyAccounting.DAL.Repositories
+{
+    class PersonsRepository:IPersonsRepository
+    {
+    }
+}
