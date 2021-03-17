0b809d4196bc50162437e6581d23512d65d82927
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2021-03-17 20:11:21 +0200

    248_Create_Entities

---
+++ FamilyAccounting.DAL/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FamilyAccounting.DAL.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
+        public IEnumerable<Wallet> Wallets { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+    }
+}
---
+++ FamilyAccounting.DAL/Entities/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FamilyAccounting.DAL.Entities
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public Person Person { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; }
+        public bool CashType { get; set; }
+        public Card Card { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expense { get; set; }
+
+    }
+}
