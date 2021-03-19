c40a51a86d404542847de23834f78fc0093ad302
iryna-onyshkevych <62593781+iryna-onyshkevych@users.noreply.github.com>
2021-03-19 14:12:54 +0200

    290_Add_update_action_to_PersonController (#32)

--- FamilyAccounting.Web/Controllers/PersonController.cs
+++ FamilyAccounting.Web/Controllers/PersonController.cs
@@ -30,6 +30,22 @@ namespace FamilyAccounting.Web.Controllers
             return View(personVM);
         }
 
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonDTO, PersonViewModel>());
+            var mapper = new Mapper(config);
+            PersonDTO updatedPerson = personsService.Get(id);
+            mapper.Map<PersonViewModel>(updatedPerson);
+            return View(updatedPerson);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, PersonDTO person)
+        {
+            PersonDTO updatedPerson = personsService.Update(id, person);
+            return RedirectToAction("Index");
+        }
         public IActionResult Details(int Id)
         {
             try
