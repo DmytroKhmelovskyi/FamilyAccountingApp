f45e4666ce49bc9b4160cf79cd79b42752dc9d36
iryna-onyshkevych <62593781+iryna-onyshkevych@users.noreply.github.com>
2021-03-20 11:33:26 +0200

    310_Create_Tests_Folders (#43)

---
+++ FamilyAccounting.Tests/ControllerTests/PersonControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using FamilyAccounting.BL.DTO;
+using FamilyAccounting.BL.Interfaces;
+using FamilyAccounting.Web.Controllers;
+using FamilyAccounting.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyAccounting.Tests.ControllerTests
+{
+    class PersonControllerTests
+    {
+       
+    }
+}
