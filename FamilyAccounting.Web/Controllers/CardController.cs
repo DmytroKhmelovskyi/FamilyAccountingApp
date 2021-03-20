5e411e0f3ca190ff1a93c72fbe7851cb0f090c7f
d_hmlvsk <d_hmlvsk@ukr.net>
2021-03-20 13:45:42 +0200

    314 Add services and mapper in WEB

--- FamilyAccounting.Web/Controllers/CardController.cs
+++ FamilyAccounting.Web/Controllers/CardController.cs
@@ -1,11 +1,5 @@
 ﻿using FamilyAccounting.BL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FamilyAccounting.Web.Controllers
 {
--- FamilyAccounting.Web/Controllers/PersonController.cs
+++ FamilyAccounting.Web/Controllers/PersonController.cs
@@ -1,21 +1,14 @@
-﻿using AutoMapper;
-using FamilyAccounting.BL.DTO;
-using FamilyAccounting.BL.Interfaces;
+﻿using FamilyAccounting.BL.Interfaces;
 using FamilyAccounting.Web.Models;
+using FamilyAccounting.Web.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FamilyAccounting.Web.Controllers
 {
     public class PersonController : Controller
     {
         private readonly IPersonService personsService;
-
         public PersonController(IPersonService personsService)
         {
             this.personsService = personsService;
@@ -25,15 +18,9 @@ namespace FamilyAccounting.Web.Controllers
         {
             try
             {
-                var personDTOs = personsService.Get();
-                var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonDTO, PersonViewModel>());
-                var mapper = new Mapper(config);
-                var personVM = mapper.Map<IEnumerable<PersonViewModel>>(personDTOs);
-                var indexVM = new IndexPersonViewModel
-                {
-                    Persons = personVM
-                };
-                return View(indexVM);
+                var person = personsService.Get();
+                var IndexVM = MapperService.PersonMap(person);
+                return View(IndexVM);
             }
             catch (Exception)
             {
@@ -51,9 +38,7 @@ namespace FamilyAccounting.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonViewModel, PersonDTO>());
-                var mapper = new Mapper(config);
-                var createdPerson = personsService.Add(mapper.Map<PersonDTO>(person));
+                var createdPerson = personsService.Add(MapperService.PersonMap(person));
                 return RedirectToAction("Index");
             }
 
@@ -63,32 +48,24 @@ namespace FamilyAccounting.Web.Controllers
         [HttpGet]
         public IActionResult Update(int id)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonDTO, PersonViewModel>());
-            var mapper = new Mapper(config);
             var updatedPerson = personsService.Get(id);
-            mapper.Map<PersonViewModel>(updatedPerson);
-            return View(mapper.Map<PersonViewModel>(updatedPerson));
+            return View(MapperService.PersonMap(updatedPerson));
         }
 
         [HttpPost]
         public IActionResult Update(int id, PersonViewModel person)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonViewModel, PersonDTO>());
-            var mapper = new Mapper(config);
-            var updatedPerson = personsService.Update(id, mapper.Map<PersonDTO>(person));
+            var updatedPerson = personsService.Update(id, MapperService.PersonMap(person));
             return RedirectToAction("Index");
         }
         public IActionResult Details(int Id)
         {
             try
             {
-                var personDTOs = personsService.Get(Id);
-                var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonDTO, PersonViewModel>());
-                var mapper = new Mapper(config);
-                var personVM = mapper.Map<PersonViewModel>(personDTOs);
-                return View(personVM);
+                var person = personsService.Get(Id);
+                return View(MapperService.PersonMap(person));
             }
-            catch(Exception)
+            catch (Exception)
             {
                 return BadRequest();
             }
@@ -98,10 +75,7 @@ namespace FamilyAccounting.Web.Controllers
         public ViewResult Delete(int? id)
         {
             var person = personsService.Get((int)id);
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<PersonDTO, PersonViewModel>());
-            var mapper = new Mapper(config);
-            var personVM = mapper.Map<PersonViewModel>(person);
-            return View(personVM);
+            return View(MapperService.PersonMap(person));
         }
 
         [ActionName("Delete")]
