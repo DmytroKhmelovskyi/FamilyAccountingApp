cc72e2015f6e363ccbd0e28ef440640c38c304db
Pain1x <33297209+Pain1x@users.noreply.github.com>
2021-03-20 16:09:53 +0200

    318_Create_View_for_wallet_details (#56)

--- FamilyAccounting.Web/Models/WalletViewModel.cs
+++ FamilyAccounting.Web/Models/WalletViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace FamilyAccounting.Web.Models
 {
     public class WalletViewModel
     {
-        
+        public int Id { get; set; }
+        public PersonViewModel Person { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+        public IEnumerable<TransactionViewModel> Transactions { get; set; }
+        public bool IsCash { get; set; }
+        public CardViewModel Card { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expense { get; set; }
+        public bool IsActive { get; set; }
     }
 }
