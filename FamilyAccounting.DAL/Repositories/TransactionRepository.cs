178939887d83d75ebaee2073ba0d7c5fe8b01e99
Nazar Chura <gravityholdontome@gmail.com>
2021-03-22 12:45:18 +0200

    327_Create_methods_to_make_expense_in_DAL

---
+++ FamilyAccounting.DAL/Repositories/TransactionRepository.cs
@@ -0,0 +1,35 @@
+﻿using FamilyAccounting.DAL.Connection;
+using FamilyAccounting.DAL.Entities;
+using FamilyAccounting.DAL.Interfaces;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FamilyAccounting.DAL.Repositories
+{
+    class TransactionRepository : ITransactionRepository
+    {
+        private readonly string connectionString;
+        public TransactionRepository(DbConfig dbConfig)
+        {
+            connectionString = dbConfig.ConnectionString;
+        }
+        public Transaction MakeExpense(Transaction transaction)
+        {
+            string sqlExpression = "PR_Wallets_Update_MakeExpense";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@_id_wallet", transaction.SourceWallet);
+                command.Parameters.AddWithValue("@_amount", transaction.Amount);
+                command.Parameters.AddWithValue("@_id_category", transaction.Category);
+                command.Parameters.AddWithValue("@_description", transaction.Description);
+                command.ExecuteNonQuery();
+            }
+            return transaction;
+        }
+    }
+}
