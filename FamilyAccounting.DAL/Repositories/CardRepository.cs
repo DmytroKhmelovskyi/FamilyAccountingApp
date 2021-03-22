ee3f685ed4bf7402f37a57287a4793b2f315457d
iryna-onyshkevych <62593781+iryna-onyshkevych@users.noreply.github.com>
2021-03-22 20:25:49 +0200

    361_Add_create_card_method_in_DAL (#80)

--- FamilyAccounting.DAL/Repositories/CardRepository.cs
+++ FamilyAccounting.DAL/Repositories/CardRepository.cs
@@ -1,11 +1,31 @@
-﻿using FamilyAccounting.DAL.Interfaces;
+﻿using FamilyAccounting.DAL.Connection;
+using FamilyAccounting.DAL.Entities;
+using FamilyAccounting.DAL.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace FamilyAccounting.DAL.Repositories
 {
-    class CardRepository : ICardRepository
+    public class CardRepository : ICardRepository
     {
+        private readonly string connectionString;
+        public CardRepository(DbConfig dbConfig)
+        {
+            connectionString = dbConfig.ConnectionString;
+        }
+
+        public Card Create(Card card)
+        {
+            string sqlExpression = $"EXEC PR_Cards_Create {card.WalletId}, '{card.Number}', '{card.Description}'";
+
+            SqlConnection sql = new SqlConnection(connectionString);
+            sql.Open();
+            SqlCommand command = new SqlCommand(sqlExpression, sql);
+            command.ExecuteNonQuery();
+            sql.Close();
+            return card;
+        }
     }
 }
