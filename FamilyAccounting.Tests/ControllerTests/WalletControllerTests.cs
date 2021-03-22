771329366c4acf61316c52be92e2c861bfc9d501
d_hmlvsk <d_hmlvsk@ukr.net>
2021-03-22 13:28:58 +0200

    336 Tests for delete wallet method

--- FamilyAccounting.Tests/ControllerTests/WalletControllerTests.cs
+++ FamilyAccounting.Tests/ControllerTests/WalletControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using FamilyAccounting.BL.Interfaces;
 using FamilyAccounting.Web.Controllers;
+using FamilyAccounting.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
@@ -71,5 +72,66 @@ namespace FamilyAccounting.Tests.ControllerTests
             Assert.IsNotNull(controller);
             Assert.AreEqual(expected, controller.GetType().Name);
         }
+        [Test]
+        public void DeleteShouldCallDeleteWalletInBlOnce()
+        {
+            //Arrange
+            var mock = new Mock<IWalletService>();
+            var controller = new WalletController(mock.Object);
+            mock.Setup(x => x.Delete(1));
+
+            //Act
+            controller.DeleteWallet(1);
+
+            //Assert
+            mock.Verify(x => x.Delete(1), Times.Once);
+        }
+
+        [Test]
+        public void DeleteShouldRedirectToActionDelete()
+        {
+            // Arrange
+            var mock = new Mock<IWalletService>();
+            var controller = new WalletController(mock.Object);
+
+            // Act
+            var result = controller.RedirectToAction("Delete");
+
+            // Assert
+            Assert.That(result.ActionName, Is.EqualTo("Delete"));
+        }
+
+        [Test]
+        public void Delete_Success_ReturnsARedirectToActionResut()
+        {
+            // Arrange
+            var walletId = 1;
+            var walletGuest = new WalletViewModel() { Id = walletId };
+            var walletRepo = new Mock<IWalletService>();
+            walletRepo.Setup(g => g.Delete(walletId));
+            var controller = new WalletController(walletRepo.Object);
+
+            // Act
+            var result = controller.DeleteWallet(walletId);
+
+            // Assert
+            var redirectToActionResult = result as RedirectToActionResult;
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        [Test]
+        public void Delete_NotNull_ResultIsNotNull()
+        {
+            //Arrange
+            var wallet = new WalletViewModel();
+            var mock = new Mock<IWalletService>();
+            mock.Setup(g => g.Delete(wallet.Id));
+            var controller = new WalletController(mock.Object);
+            //Act
+            ViewResult result = controller.Delete(wallet.Id) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
+
