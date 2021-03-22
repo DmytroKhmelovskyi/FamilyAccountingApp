b7e0e26f31ea4e3f8ac2018dc6e863c6bfe5e243
iryna-onyshkevych <62593781+iryna-onyshkevych@users.noreply.github.com>
2021-03-22 21:06:21 +0200

    362_Add_create_card_method_in_BL (#81)

--- FamilyAccounting.BL/Services/CardService.cs
+++ FamilyAccounting.BL/Services/CardService.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using FamilyAccounting.BL.DTO;
 using FamilyAccounting.BL.Interfaces;
+using FamilyAccounting.DAL.Entities;
 using FamilyAccounting.DAL.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -17,6 +19,18 @@ namespace FamilyAccounting.BL.Services
         {
             this.mapper = mapper;
             this.cardsRepository = cardsRepository;
+        }
+        public CardDTO Create(CardDTO card)
+        {
+            try
+            {
+                Card _card = cardsRepository.Create(mapper.Map<Card>(card));
+                return mapper.Map<CardDTO>(_card);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
