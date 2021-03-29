beb25aa9762467285e337cb888288b74de5ea0ea
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2021-03-29 23:00:47 +0300

    430_Add_tests_to_create_card_method

---
+++ FamilyAccounting.Tests/ServiceTests/CardServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using FamilyAccounting.BL.DTO;
+using FamilyAccounting.BL.Interfaces;
+using FamilyAccounting.BL.Services;
+using FamilyAccounting.DAL.Connection;
+using FamilyAccounting.DAL.Entities;
+using FamilyAccounting.DAL.Interfaces;
+using FamilyAccounting.DAL.Repositories;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyAccounting.Tests.ServiceTests
+{
+    public class CardServiceTests
+    {
+
+        [Test]
+        public void CardService_CreateAnObject()
+        {
+            //Arrange
+            DbConfig dbConfig = new DbConfig();
+            ICardRepository cardRepository = new CardRepository(dbConfig);
+            var mock = new Mock<IMapper>();
+            string expected = "CardService";
+
+            //Act
+            CardService cardService = new CardService(mock.Object, cardRepository);
+
+            //Assert
+            Assert.IsNotNull(cardService);
+            Assert.AreEqual(expected, cardService.GetType().Name);
+        }
+
+        [Test]
+        public void CreateCard_ShouldCallCreateInDalOnce()
+        {
+            //Arrange
+            CardDTO cardDTO = new CardDTO
+            {
+                Description = "Description",
+            };
+            var mockMapper = new Mock<IMapper>();
+            var mockRepository = new Mock<ICardRepository>();
+            ICardService service = new CardService(mockMapper.Object, mockRepository.Object);
+            mockRepository.Setup(x => x.Create(It.IsAny<Card>())).Returns(It.IsAny<Card>());
+
+            //Act
+            service.Create(cardDTO);
+
+            //Assert
+            mockRepository.Verify(x => x.Create(It.IsAny<Card>()), Times.Once);
+        }
+
+        [Test]
+        public void CreateCard_ShouldReturnCardDTO()
+        {
+            //Arrange
+            CardDTO shouldBe = new CardDTO
+            {
+                Description = "Description",
+            };
+
+            CardDTO cardDTO = new CardDTO
+            {
+                Description = "Description",
+            };
+            var mock = new Mock<ICardService>();
+            mock.Setup(x => x.Create(cardDTO)).Returns(cardDTO);
+
+            //Act
+            var result = mock.Object.Create(cardDTO);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(shouldBe.GetType().Name, result.GetType().Name);
+        }
+    }
+}
