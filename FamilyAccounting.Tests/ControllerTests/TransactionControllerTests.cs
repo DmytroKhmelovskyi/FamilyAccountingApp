ba86361a3b024957b98f8d4adc2590d3549c8a0a
d_hmlvsk <d_hmlvsk@ukr.net>
2021-03-31 01:08:50 +0300

    469  Add unit tests for income methods

--- FamilyAccounting.Tests/ControllerTests/TransactionControllerTests.cs
+++ FamilyAccounting.Tests/ControllerTests/TransactionControllerTests.cs
@@ -18,11 +18,52 @@ namespace FamilyAccounting.Tests.ControllerTests
             var mock2 = new Mock<IWalletWebService>();
 
             //Act
-            TransactionController controller = new TransactionController(mock.Object, mock2.Object); 
+            TransactionController controller = new TransactionController(mock.Object, mock2.Object);
 
             //Assert
             Assert.IsNotNull(controller);
             Assert.AreEqual(expected, controller.GetType().Name);
         }
+
+        [Test]
+        public void Update_ReturnsRedirect_ToActionResut()
+        {
+            // Arrange
+            var transactionId = 1;
+            var transaction = new TransactionViewModel() { Id = transactionId };
+            var mockTransactions = new Mock<ITransactionWebService>();
+            var mockWallets = new Mock<IWalletWebService>();
+            mockTransactions.Setup(p => p.Update(transactionId, It.IsAny<TransactionViewModel>()));
+            var controller = new TransactionController(mockTransactions.Object, mockWallets.Object);
+
+            // Act
+            var result = controller.Update(transactionId, transaction);
+
+            // Assert
+            var redirectToActionResult = result as RedirectToActionResult;
+            Assert.AreEqual("Details", redirectToActionResult.ActionName);
+        }
+
+        [Test]
+        public void Update_NotNull_ViewResultIsNotNull()
+        {
+            //Arrange
+            var transaction = new TransactionViewModel()
+            {
+                Id = 1,
+                Description = "Transaction",
+                CategoryId = 1
+            };
+            var mockTransactions = new Mock<ITransactionWebService>();
+            var mockWallets = new Mock<IWalletWebService>();
+            var controller = new TransactionController(mockTransactions.Object, mockWallets.Object);
+
+            //Act
+            var result = controller.Update(transaction.Id, transaction);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
     }
+
 }
