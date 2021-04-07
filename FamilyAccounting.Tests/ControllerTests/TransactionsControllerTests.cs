e756e68b9bd5b94af60af7148238f432de6d1a4d
Pain1x <33297209+Pain1x@users.noreply.github.com>
2021-04-07 10:47:24 +0300

    Update_methods_and_tests (#171)

--- FamilyAccounting.Tests/ControllerTests/TransactionsControllerTests.cs
+++ FamilyAccounting.Tests/ControllerTests/TransactionsControllerTests.cs
@@ -11,20 +11,7 @@ namespace FamilyAccounting.Tests.ControllerTests
 {
     class TransactionsControllerTests
     {
-        [Test]
-        public void TransactionDetails_StatusCodeShouldBeOk()
-        {
-            //Arrange
-            var client = new RestClient("https://localhost:44398/api/Transactions/Details/85?walletId=12&transactionId=85");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.GET);
-
-            //Act
-            IRestResponse response = client.Execute(request);
-
-            //Assert
-            Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
-        }  
+  
     }
 }
 
