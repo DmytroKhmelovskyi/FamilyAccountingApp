0451418118da6454dd935b662e75b52e235a9376
Iryna Onyshkevych <onyshkevych.iryna@gmail.com>
2021-04-15 13:10:13 +0300

    add_person_details_in_ReactJS

--- FamilyAccounting.ReactJS/ClientApp/src/components/PersonDetails.js
+++ FamilyAccounting.ReactJS/ClientApp/src/components/PersonDetails.js
@@ -7,15 +7,7 @@ export class PersonDetails extends React.Component {
 
         this.state = {
             empData: {}, loading: true ,};
-        //this.handleDetails = this.handleDetails.bind(this);  
-
     }
-    //render() {
-    //    // получаем параметры
-    //    const personid = this.props.match.params.personid;
-    //    return <h2>id: {personid}  Name:persons. </h2>;
-    //}
-
 
     componentDidMount() {
         this.populatePersonsData();
@@ -65,46 +57,3 @@ export class PersonDetails extends React.Component {
         this.setState({ empData: data, loading: false });
     }
 }
-//export class PersonDetails extends Component {
-//    static displayName = PersonDetails.name;
-
-//    constructor(props) {
-//        super(props);
-//        this.state = { persons: [], loading: true };
-//        //this.handleDetails = this.handleDetails.bind(this);  
-
-//    }
-
-//    componentDidMount() {
-//        this.populatePersonsData();
-//    }
-
-
-//    // handleDetails(id) {
-//    //    this.props.history.push("/persons/details/" + id);
-//    //}  
-
-
-   
-
-//    render() {
-//        console.log(API_BASE_URL)
-//        let contents = this.state.loading
-//            ? <p><em>Loading...</em></p>
-//            : PersonDetails.renderPersonsData(this.state.persons);
-
-//        return (
-//            <div>
-//                <h1 id="tabelLabel" >Persons</h1>
-//                <p>This component demonstrates fetching data from the server.</p>
-//                {contents}
-//            </div>
-//        );
-//    }
-
-//    async populatePersonsData() {
-//        const response = await fetch(`${API_BASE_URL}/persons/details/:personid`);
-//        const data = await response.json();
-//        this.setState({ persons: data, loading: false });
-//    }
-//}
