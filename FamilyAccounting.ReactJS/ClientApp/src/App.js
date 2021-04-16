2907ba2354e42cacd37623a9300ca88f00e76bcf
d_hmlvsk <d_hmlvsk@ukr.net>
2021-04-16 20:44:54 +0300

    addperson mehod in react app

--- FamilyAccounting.ReactJS/ClientApp/src/App.js
+++ FamilyAccounting.ReactJS/ClientApp/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Person } from './components/Person';
 import { PersonDetails } from './components/PersonDetails';
 import { WalletDetails } from './components/WalletDetails';
-import { Transactions } from './components/ListOfTransactions';
+import { PersonAdd } from './components/PersonAdd';
 
 import './custom.css'
 
@@ -19,7 +20,7 @@ export default class App extends Component {
             <Route exact path='/persons' component={Person} />
             <Route exact path='/persons/details/:personid' component={PersonDetails} />
             <Route exact path='/wallets/details/:walletid' component={WalletDetails} />
-            <Route exact path='/transactionz/:walletid' component={Transactions} />
+            <Route exact path='/persons/add' component={PersonAdd} />
       </Layout>
     );
   }
---
+++ FamilyAccounting.ReactJS/ClientApp/src/components/PersonAdd.js
@@ -0,0 +1,141 @@
+﻿import React from 'react';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+import { Container, Col, Form, Row, FormGroup, Label, Input, Button }
+    from 'reactstrap';
+export class PersonAdd extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            FirstName: '',
+            LastName: '',
+            Email: '',
+            Phone: ''
+        }
+    }
+    Addperson = () => {
+        axios.post(`${API_BASE_URL}/persons/add/`, {
+            FirstName: this.state.FirstName, LastName: this.state.LastName,
+            Email: this.state.Email, Phone: this.state.Phone
+        })
+            .then(json => {
+                if (json.data.Status === 'Success') {
+                    console.log(json.data.Status);
+                    alert("Data Save Successfully");
+                    this.props.history.push('/persons')
+                }
+                else {
+                    alert('Data not Saved');
+                    debugger;
+                    this.props.history.push('/persons')
+                }
+            })
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        return (
+
+            < Container className="App" >
+
+                < h4 className="PageHeading" > Enter Student Informations</ h4 >
+
+                < Form className="form" >
+
+                    < Col >
+
+                        < FormGroup row >
+
+                            < Label for="FirstName" sm={2}> FirstName </ Label >
+
+                            < Col sm={10}>
+
+                                < Input type="text" name="FirstName" onChange={this.handleChange}
+                                    value={this.state.FirstName}
+                                    placeholder="Enter FirstName" />
+
+                            </ Col >
+
+                        </ FormGroup >
+
+                        < FormGroup row >
+
+                            < Label for="LastName" sm={2}> LastName </ Label >
+
+                            < Col sm={10}>
+
+                                < Input type="text" name="LastName" onChange={this.handleChange}
+                                    value={this.state.LastName}
+                                    placeholder="Enter LastName" />
+
+                            </ Col >
+
+                        </ FormGroup >
+
+                        < FormGroup row >
+
+                            < Label for="Email" sm={2}> Email </ Label >
+
+                            < Col sm={10}>
+
+                                < Input type="text" name="Email" onChange={this.handleChange}
+                                    value={this.state.Email}
+                                    placeholder="Enter Email" />
+
+                            </ Col >
+
+                        </ FormGroup >
+
+                        < FormGroup row >
+
+                            < Label for="Phone" sm={2}> Phone </ Label >
+
+                            < Col sm={10}>
+
+                                < Input type="text" name="Phone" onChange={this.handleChange}
+                                    value={this.state.Phone}
+                                    placeholder="Enter Phone" />
+
+                            </ Col >
+
+                        </ FormGroup >
+
+                    </ Col >
+
+                    < Col >
+
+                        < FormGroup row >
+
+                            < Col sm={5}>
+
+                            </ Col >
+
+                            < Col sm={1}>
+
+                                < button type="button" onClick={this.Addperson}
+                                    className="btn btn-success" > Submit </ button >
+
+                            </ Col >
+
+                            < Col sm={1}>
+
+                                < Button color="danger" > Cancel </ Button >{' '}
+                            </ Col >
+                            < Col sm={5}>
+
+                            </ Col >
+
+                        </ FormGroup >
+
+                    </ Col >
+
+                </ Form >
+
+            </ Container >
+        );
+    }
+
+}
